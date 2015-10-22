48a305818723ec4a4afbe369cc74b6970e39cfa3
Paul Walker <wpaulsg@gmail.com>
2015-10-21 21:37:07 -0500

    rdatesync-test.pl: replaced system rm/mkdir with subs

--- rdatesync-test.pl
+++ rdatesync-test.pl
@@ -14,9 +14,17 @@ my $ASSERT_FAIL = 0;
 my $ASSERT_PASS = 0;
 # Flag for if we bail out before all tests
 my $FATAL_ERROR = 1;
+END {
+	print "\n";
+	print "Test success: $TEST_PASS\n";
+	print "Test failure: $TEST_FAIL\n";
+	print "Assert pass: $ASSERT_PASS\n";
+	print "Assert fail: $ASSERT_FAIL\n";
 
-my $RM = "/bin/rm -rf";
-my $MKDIR = "/bin/mkdir -p";
+	if ($FATAL_ERROR) {
+		print "\nTests aborted due to a fatal error\n";
+	}
+}
 
 sub report_test_pass {
 	$TEST_PASS++;
@@ -83,6 +91,28 @@ sub assert_match {
 
 
 
+sub mv {
+	system("/bin/mv -f " . join(' ', @_));
+}
+sub inode {
+	my $inode = `/bin/ls -i $_[0]`;
+	chomp($inode);
+	return (split(/\s+/, $inode))[0];
+}
+sub md5sum {
+	my $md5 = `/usr/bin/md5sum $_[0]`;
+	chomp($md5);
+	return (split(/\s+/, $md5))[0];
+}
+sub mkdir {
+	system("/bin/mkdir -p " . join(' ', @_));
+}
+sub rm {
+	system("/bin/rm -rf " . join(' ', @_));
+}
+
+
+
 sub test_sanity {
 	my $pass_copy = $ASSERT_PASS;
 	my $fail_copy = $ASSERT_FAIL;
@@ -113,7 +143,7 @@ sub test_sanity {
 	&assert_not_file($test_file) and return &report_test_fail("System failed to create test file");
 	$fail_count++;
 
-	system("$RM $test_file");
+	&rm($test_file);
 	&assert_file($test_file) and return &report_test_fail("System failed to remove test file");
 	$fail_count++;
 	&assert_not_file($test_file) or return &report_test_fail("System failed to remove test file");
@@ -125,13 +155,13 @@ sub test_sanity {
 	&assert_not_dir($test_dir) or return &report_test_fail("Test directory exists already");
 	$pass_count++;
 
-	system("$MKDIR $test_dir");
+	&mkdir($test_dir);
 	&assert_dir($test_dir) or return &report_test_fail("System failed to create test directory");
 	$pass_count++;
 	&assert_not_dir($test_dir) and return &report_test_fail("System failed to create test directory");
 	$fail_count++;
 
-	system("$RM $test_dir");
+	&rm($test_dir);
 	&assert_dir($test_dir) and return &report_test_fail("System failed to remove test directory");
 	$fail_count++;
 	&assert_not_dir($test_dir) or return &report_test_fail("System failed to remove test directory");
@@ -148,64 +178,92 @@ sub test_sanity {
 
 	return &report_test_pass();
 }
+&test_sanity() or die "Test suite failed sanity check";
+
+
 
 sub test_first_run {
 	my $workspace = "/tmp/_test_sandbox";
 	my $backup_to_here = "$workspace/backups";
-	my $backup_name = "ThisIsMyBackup";
-	my $backup_this = "$workspace/$backup_name";
+	my $backup_dir_name = "ThisIsMyBackup";
+	my $backup_this_dir = "$workspace/$backup_dir_name";
+	my $backup_file_name = "aRandomFile";
+	my $backup_this_file = "$backup_this_dir/$backup_file_name";
+	my $file_md5sum;
 	my $test_conf = "$workspace/_test_conf";
 	my $date_today = `date +%Y-%m-%d`;
+	my $date_yesterday = `date --date="yesterday" +%Y-%m-%d`;
 
 	chomp($date_today);
+	chomp($date_yesterday);
 
 	&assert_not_dir($workspace) or &report_test_fail("Test workspace already present");
 
 	&assert_match($RDATESYNC, 'rdatesync.pl$');
 	&assert_file($RDATESYNC);
 
-	system("$MKDIR $workspace");
-	system("$MKDIR $backup_this");
-
-	open(my $tfh, '>', $test_conf) or (system("$RM $workspace") and &report_test_fail("Failed to open test configuration for writing"));
-	print $tfh "destination $backup_to_here\n";
-	print $tfh "backup $backup_this\n";
+	&mkdir($workspace);
+	&mkdir($backup_this_dir);
+	open(my $tfh, '>', $backup_this_file) or (&rm($workspace) and &report_test_fail("Failed to open test file for writing"));
+	for (1..100) {
+		print $tfh int(rand(10));
+	}
 	close($tfh);
+	$file_md5sum = &md5sum($backup_this_file);
 
-	print "First Run Test: Starting $RDATESYNC\n\n";
-	system("perl $RDATESYNC $test_conf 2>&1 | sed 's/^/    /'");
-	print "\nFirst Run Test: $RDATESYNC Finished\n\n";
+	open(my $cfh, '>', $test_conf) or (&rm($workspace) and &report_test_fail("Failed to open test configuration for writing"));
+	print $cfh "destination $backup_to_here\n";
+	print $cfh "backup $backup_this_dir\n";
+	close($cfh);
 
-	# If this fails, see how badly it failed
-	if (! &assert_dir("$backup_to_here/daily/$date_today/$backup_name")) {
-		if (! &assert_dir("$backup_to_here/daily/$date_today")) {
-			if (! &assert_dir("$backup_to_here/daily/")) {
-				&assert_dir("$backup_to_here")
+	system("perl $RDATESYNC $test_conf >/dev/null 2>&1");
+
+	if (! &assert_equal(&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"), $file_md5sum)) {
+		# see how badly it failed
+		if (! &assert_file("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name")) {
+			if (! &assert_dir("$backup_to_here/daily/$date_today/$backup_dir_name")) {
+				if (! &assert_dir("$backup_to_here/daily/$date_today")) {
+					if (! &assert_dir("$backup_to_here/daily/")) {
+						&assert_dir("$backup_to_here")
+					}
+				}
 			}
 		}
+		&rm($workspace);
+		return &report_test_fail("Failed to create first backup");
+	}
+
+	&mv("$backup_to_here/daily/$date_today", "$backup_to_here/daily/$date_yesterday");
+
+	system("perl $RDATESYNC $test_conf 2>&1 | sed 's/^/    /'");
+
+	if (! &assert_equal(
+			&inode("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"),
+			&inode("$backup_to_here/daily/$date_yesterday/$backup_dir_name/$backup_file_name"))) {
+		# see how badly it failed
+		if (! &assert_equal(&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"), $file_md5sum)) {
+			if (! &assert_file("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name")) {
+				if (! &assert_dir("$backup_to_here/daily/$date_today/$backup_dir_name")) {
+					if (! &assert_dir("$backup_to_here/daily/$date_today")) {
+						if (! &assert_dir("$backup_to_here/daily/")) {
+							&assert_dir("$backup_to_here")
+						}
+					}
+				}
+			}
+		}
+		&rm($workspace);
+		return &report_test_fail("Failed to seed today's backup from yesterday");
 	}
 
 
 
-	system("$RM $workspace");
+	&rm($workspace);
 	&report_test_pass();
 }
 
-END {
-	print "\n";
-	print "Test success: $TEST_PASS\n";
-	print "Test failure: $TEST_FAIL\n";
-	print "Assert pass: $ASSERT_PASS\n";
-	print "Assert fail: $ASSERT_FAIL\n";
-
-	if ($FATAL_ERROR) {
-		print "\nTests aborted due to a fatal error\n";
-	}
-}
 
 
-
-&test_sanity() or die "Test suite failed sanity check";
 print "Sanity test passed\n\n";
 &test_first_run();
 
