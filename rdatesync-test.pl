f9eccec6031a804ee30490443908dfbdbc52e412
Paul Walker <wpaulsg@gmail.com>
2015-10-21 23:29:36 -0500

    rdatesync-test.pl: added test for new files going in

--- rdatesync-test.pl
+++ rdatesync-test.pl
@@ -52,6 +52,14 @@ sub assert_equal {
 	}
 	return &report_assert_pass();
 }
+sub assert_not_equal {
+	my $left = shift;
+	my $right = shift;
+	if ($left eq $right) {
+		return &report_assert_fail("'$left' eq '$right'");
+	}
+	return &report_assert_pass();
+}
 sub assert_file {
 	my $file = shift;
 	if (! -f $file) {
@@ -124,10 +132,16 @@ sub test_sanity {
 	# Generate some assert success and failures
 	&assert_equal(1, 1);
 	$pass_count++;
+	&assert_not_equal(1, 1);
+	$fail_count++;
 
 	&assert_equal(1, '1');
 	$pass_count++;
+	&assert_not_equal(1, '1');
+	$fail_count++;
 
+	&assert_not_equal(1, 'one');
+	$pass_count++;
 	&assert_equal(1, 'one');
 	$fail_count++;
 
@@ -176,10 +190,19 @@ sub test_sanity {
 	$ASSERT_PASS = $pass_copy;
 	$ASSERT_FAIL = $fail_copy;
 
-	return &report_test_pass();
+	return 1;
 }
 &test_sanity() or die "Test suite failed sanity check";
+print "Sanity test passed\n\n";
 
+
+my $WORKSPACE = "/tmp/_test_sandbox";
+my $BACKUP_TO_HERE = "$WORKSPACE/backups"
+
+sub setup_test {
+	&rm($WORKSPACE);
+	&mkdir($WORKSPACE);
+}
 
 
 sub test_first_run {
@@ -189,13 +212,14 @@ sub test_first_run {
 	my $backup_this_dir = "$workspace/$backup_dir_name";
 	my $backup_file_name = "aRandomFile";
 	my $backup_this_file = "$backup_this_dir/$backup_file_name";
-	my $file_md5sum;
 	my $test_conf = "$workspace/_test_conf";
 	my $date_today = `date +%Y-%m-%d`;
 	my $date_yesterday = `date --date="yesterday" +%Y-%m-%d`;
+	my $date_last_month = `date --date="1 month ago" +%Y-%m-%d`;
 
 	chomp($date_today);
 	chomp($date_yesterday);
+	chomp($date_last_month);
 
 	&assert_not_dir($workspace) or &report_test_fail("Test workspace already present");
 
@@ -209,7 +233,6 @@ sub test_first_run {
 		print $tfh int(rand(10));
 	}
 	close($tfh);
-	$file_md5sum = &md5sum($backup_this_file);
 
 	open(my $cfh, '>', $test_conf) or (&rm($workspace) and &report_test_fail("Failed to open test configuration for writing"));
 	print $cfh "destination $backup_to_here\n";
@@ -218,7 +241,9 @@ sub test_first_run {
 
 	system("perl $RDATESYNC $test_conf >/dev/null 2>&1");
 
-	if (! &assert_equal(&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"), $file_md5sum)) {
+	if (! &assert_equal(
+			&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"),
+			&md5sum($backup_this_file))) {
 		# see how badly it failed
 		if (! &assert_file("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name")) {
 			if (! &assert_dir("$backup_to_here/daily/$date_today/$backup_dir_name")) {
@@ -235,13 +260,15 @@ sub test_first_run {
 
 	&mv("$backup_to_here/daily/$date_today", "$backup_to_here/daily/$date_yesterday");
 
-	system("perl $RDATESYNC $test_conf 2>&1 | sed 's/^/    /'");
+	system("perl $RDATESYNC $test_conf >/dev/null 2>&1");
 
 	if (! &assert_equal(
 			&inode("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"),
 			&inode("$backup_to_here/daily/$date_yesterday/$backup_dir_name/$backup_file_name"))) {
 		# see how badly it failed
-		if (! &assert_equal(&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"), $file_md5sum)) {
+		if (! &assert_equal(
+				&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"),
+				&md5sum($backup_this_file))) {
 			if (! &assert_file("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name")) {
 				if (! &assert_dir("$backup_to_here/daily/$date_today/$backup_dir_name")) {
 					if (! &assert_dir("$backup_to_here/daily/$date_today")) {
@@ -253,8 +280,47 @@ sub test_first_run {
 			}
 		}
 		&rm($workspace);
-		return &report_test_fail("Failed to seed today's backup from yesterday");
+		return &report_test_fail("Failed to duplicate backups from yesterday");
 	}
+
+	&rm("$backup_to_here/daily/$date_today");
+	&mv("$backup_to_here/daily/$date_yesterday", "$backup_to_here/daily/$date_last_month");
+
+	system("perl $RDATESYNC $test_conf >/dev/null 2>&1");
+
+	# rdatesync should move last month's dir to monthly backups
+	if (! &assert_equal(
+			&inode("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"),
+			&inode("$backup_to_here/monthly/$date_last_month/$backup_dir_name/$backup_file_name"))) {
+		# see how badly it failed
+		if (! &assert_equal(
+				&md5sum("$backup_to_here/monthly/$date_today/$backup_dir_name/$backup_file_name"),
+				&md5sum($backup_this_file))) {
+			if (! &assert_file("$backup_to_here/monthly/$date_today/$backup_dir_name/$backup_file_name")) {
+				if (! &assert_dir("$backup_to_here/monthly/$date_today/$backup_dir_name")) {
+					if (! &assert_dir("$backup_to_here/monthly/$date_today")) {
+						&assert_dir("$backup_to_here/monthly/")
+					}
+				}
+			}
+		}
+		&rm($workspace);
+		return &report_test_fail("Failed to duplicate backups from yesterday");
+	}
+
+	&rm("$backup_to_here/daily/$date_today");
+	system("echo ' . int(rand(10)) . ' >> $backup_this_file");
+
+	system("perl $RDATESYNC $test_conf >/dev/null 2>&1");
+
+	&assert_equal(
+		&md5sum($backup_this_file),
+		&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name")
+	)
+	&assert_not_equal(
+		&md5sum("$backup_to_here/daily/$date_today/$backup_dir_name/$backup_file_name"),
+		&md5sum("$backup_to_here/monthly/$date_last_month/$backup_dir_name/$backup_file_name")
+	)
 
 
 
@@ -264,8 +330,8 @@ sub test_first_run {
 
 
 
-print "Sanity test passed\n\n";
 &test_first_run();
+
 
 
 $FATAL_ERROR = 0;
